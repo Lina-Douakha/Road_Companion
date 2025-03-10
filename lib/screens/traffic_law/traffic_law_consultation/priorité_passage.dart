f3791ed070cde9879d631900fc3192abfe5806ee
Amina3524 <na_meliani@esi.dz>
2025-03-10 10:30:38 +0100

    traffic laws

--- lib/screens/traffic_law/traffic_law_consultation/priorité_passage.dart
+++ lib/screens/traffic_law/traffic_law_consultation/priorité_passage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:road_companion/widgets/navigation_bar_widget.dart';
 
 class PriorityQuestionScreen extends StatefulWidget {
@@ -7,30 +9,60 @@ class PriorityQuestionScreen extends StatefulWidget {
 }
 
 class _PriorityQuestionScreenState extends State<PriorityQuestionScreen> {
-  // Liste des questions et réponses
-  final List<Map<String, String>> questions = [
-    {
-      "imageUrl": "assets/images/prioritéAlgerien.jpg",
-      "question": "Qui a la priorité à cette intersection ?",
-      "answer":
-      "Intersection avec un panneau stop à 150 mètres :\n Un panneau stop est placé à 150 mètres.\nLes voitures bleue et jaune passent en même temps, suivies par la voiture rouge",
-    },
-    {
-      "imageUrl": "assets/images/prioritéAlgerien2.jpg",
-      "question": "Qui a la priorité à cette intersection ?",
-      "answer": " Le véhicule rouge et le véhicule bleu ont la priorité.",
-    },
-  ];
-
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> questions = [];
   int currentIndex = 0;
   bool showAnswer = false;
+  bool isLoading = true;
 
-  // Fonction pour aller à la question suivante
+  @override
+  void initState() {
+    super.initState();
+    _fetchPriorityQuestions();
+  }
+
+  //Fetch priority questions from Firestore
+  Future<void> _fetchPriorityQuestions() async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore
+          .collection("Traffic_Laws")
+          .where("Category", isEqualTo: "priorities") // Filter for priority questions
+          .get();
+
+      List<Map<String, dynamic>> fetchedQuestions = querySnapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+
+      setState(() {
+        questions = fetchedQuestions;
+        isLoading = false; // Stop loading indicator
+      });
+    } catch (e) {
+      print("Error fetching priority questions: $e");
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // Go to next question
   void nextQuestion() {
-    setState(() {
-      currentIndex = (currentIndex + 1) % questions.length;
-      showAnswer = false; // Masquer la réponse pour la nouvelle question
-    });
+    if (questions.isNotEmpty) {
+      setState(() {
+        currentIndex = (currentIndex + 1) % questions.length;
+        showAnswer = false; // Hide answer when switching
+      });
+    }
+  }
+
+  // Go to previous question
+  void previousQuestion() {
+    if (questions.isNotEmpty) {
+      setState(() {
+        currentIndex = (currentIndex - 1 + questions.length) % questions.length;
+        showAnswer = false; // Hide answer when switching
+      });
+    }
   }
 
   int _selectedIndex = 0;
@@ -40,159 +72,160 @@ class _PriorityQuestionScreenState extends State<PriorityQuestionScreen> {
     });
   }
 
-  // Fonction pour revenir à la question précédente
-  void previousQuestion() {
-    setState(() {
-      currentIndex = (currentIndex - 1 + questions.length) % questions.length;
-      showAnswer = false; // Masquer la réponse pour la nouvelle question
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final questionData = questions[currentIndex];
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
+    final screenHeight = MediaQuery.of(context).size.height;
 
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 90),
-                    const Text(
-                      'Priorité de passage',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF1B9169),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-
-                    // Vérification de l'existence de l'image
-                    if (questionData.containsKey("imageUrl"))
-                      Image.asset(
-                        questionData["imageUrl"]!,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-
-                    SizedBox(height: 40),
-
-                    // Champ de texte pour la question
-                    SizedBox(
-                      width: 350,
-                      height: 60,
-                      child: TextField(
-                        controller: TextEditingController(
-                          text:
-                          questionData["question"] ??
-                              "Question non disponible",
-                        ),
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(color: Color(0xFFCBD5E1)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(color: Color(0xFFCBD5E1)),
-                          ),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Color(0xFF1B9169),
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: isLoading
+              ? Center(child: CircularProgressIndicator()) // Show loading indicator
+              : questions.isEmpty
+              ? Center(child: Text("Aucune question de priorité trouvée."))
+              : Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: screenHeight * 0.09),
+                      const Text(
+                        'Priorité de passage',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1B9169),
                         ),
                       ),
-                    ),
+                      SizedBox(height: 20),
 
-                    SizedBox(height: 30),
+                      // Show image if exists (Load from assets instead of network)
+                      if (questions[currentIndex]["imageURL"] != null &&
+                          questions[currentIndex]["imageURL"].isNotEmpty)
+                        Image.asset(
+                          "assets/images/${questions[currentIndex]["imageURL"]}",
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
 
-                    // Bouton pour afficher la réponse
-                    SizedBox(
-                      width: 300,
-                      height: 55,
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          setState(() {
-                            showAnswer = !showAnswer;
-                          });
-                        },
-                        label: Text(
-                          "Afficher la réponse",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16,
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFD1FADF),
-                          foregroundColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                        icon: Icon(
-                          showAnswer ? Icons.expand_less : Icons.expand_more,
-                          color: Color(0xFF00D47E),
-                          size: 30,
-                        ),
-                      ),
-                    ),
 
-                    // Affichage conditionnel de la réponse
-                    if (showAnswer)
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          questionData["answer"] ?? "Réponse non disponible",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFF000000),
+                      SizedBox(height: 40),
+
+                      // Show Question
+                      SizedBox(
+                        width: 350,
+                        height: 60,
+                        child: TextField(
+                          controller: TextEditingController(
+                            text: questions[currentIndex]["Category"] ??
+                                "Question non disponible",
                           ),
-                          textAlign: TextAlign.center,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(color: Color(0xFFCBD5E1)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(color: Color(0xFFCBD5E1)),
+                            ),
+                          ),
                         ),
                       ),
 
-                    SizedBox(
-                      height: 40,
-                    ), // Ajout d'un espace pour éviter le chevauchement
-                  ],
+                      SizedBox(height: 30),
+
+                      // Show Answer Button
+                      SizedBox(
+                        width: 300,
+                        height: 55,
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {
+                              showAnswer = !showAnswer;
+                            });
+                          },
+                          label: Text(
+                            "Afficher la réponse",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFD1FADF),
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          icon: Icon(
+                            showAnswer ? Icons.expand_less : Icons.expand_more,
+                            color: Color(0xFF00D47E),
+                            size: 30,
+                          ),
+                        ),
+                      ),
+
+                      // Show Answer if button is pressed
+                      if (showAnswer)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            questions[currentIndex]["Description"] ?? "Réponse non disponible",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF000000),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+
+                      SizedBox(height: 40),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            // Boutons pour naviguer entre les questions
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: previousQuestion,
-                  icon: Icon(Icons.arrow_circle_left_outlined, size: 30),
-                  color: Color(0xFF1B9169),
-                  tooltip: "Question précédente",
-                ),
-                IconButton(
-                  onPressed: nextQuestion,
-                  icon: Icon(Icons.arrow_circle_right_outlined, size: 30),
-                  color: Color(0xFF1B9169),
-                  tooltip: "Question suivante",
-                ),
-              ],
-            ),
-          ],
+              // Navigation Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: previousQuestion,
+                    icon: Icon(Icons.arrow_circle_left_outlined, size: 30),
+                    color: Color(0xFF1B9169),
+                    tooltip: "Question précédente",
+                  ),
+                  IconButton(
+                    onPressed: nextQuestion,
+                    icon: Icon(Icons.arrow_circle_right_outlined, size: 30),
+                    color: Color(0xFF1B9169),
+                    tooltip: "Question suivante",
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
 
-      bottomNavigationBar: NavigationBarWidget(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
+        bottomNavigationBar: NavigationBarWidget(
+          selectedIndex: _selectedIndex,
+          onItemTapped: _onItemTapped,
+        ),
       ),
     );
   }
